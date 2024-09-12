5f33e07695638d51f8c3dac9ed13443404e2f0f7
N0ts <mail@n0ts.top>
2024-09-12 17:57:19 +0800

    还在研究

--- mi.js
+++ mi.js
@@ -1,7 +1,13 @@
 /*
  * @Author: N0ts
  * @Date: 2024-09-11 10:48:50
- * @Description: test
+ * @Description: 监听手机通知触发自动化操作
+ * 举例：监听小米智能家居的通知，自动解锁手机，打开QQ，发送信息到指定人
+ * 如手机通知有内容为：QQ，名字，内容1，内容2，内容3
+ * 则会打开 QQ，根据当前屏幕找到含有这个名字的内容点击，然后发送内容1，内容2，内容3，最后息屏；
+ * 目前支持格式：
+ * 无密码： QQ，名字，内容1，内容2，内容3
+ * 有密码：QQ，密码，名字，内容1，内容2，内容3
  * @FilePath: \autoxjs\mi.js
  * @Mail: mail@n0ts.top
  */
@@ -9,8 +15,13 @@
 auto.waitFor();
 toast("插件已启用");
 
+// 监听哪些包的消息，白名单
 const whiteObservePackages = ["com.tencent.mobileqq", "com.xiaomi.smarthome"];
 
+// 触发密码（可以不设置）
+const pwd = "123";
+
+// 手机锁屏密码按键坐标
 const lockNumberXY = {
     1: [260, 1380],
     2: [540, 1380],
@@ -24,6 +35,7 @@ const lockNumberXY = {
     0: [540, 1920]
 };
 
+// 解锁手机的步骤
 const unlockPhone = [
     () => {
         device.wakeUpIfNeeded();
@@ -45,6 +57,7 @@ const unlockPhone = [
     }
 ];
 
+// QQ 发送信息到具体人的步骤
 const qqSendMsg = [
     () => {
         const launchResult = launch("com.tencent.mobileqq");
@@ -52,7 +65,6 @@ const qqSendMsg = [
             toast("启动QQ失败");
             return false;
         }
-        console.log("成功打开 QQ");
         return 1000;
     },
     (params) => {
@@ -89,25 +101,33 @@ function startStep(step, params) {
     return true;
 }
 
-const operate = {
+// 应用操作配置
+const AppOperate = {
     QQ: (params) => {
         startStep(qqSendMsg, params);
     }
 };
 
+// 消息监听
 events.observeNotification();
 events.onNotification(function (n) {
+    // n.delete();
     if (!whiteObservePackages.includes(n.getPackageName())) return;
     const info = n.getText().split("，");
-    if (!operate[info[0]]) return;
+    if (pwd && info[1] != pwd) {
+        console.log("密码错误，拒绝执行！", info);
+        return;
+    }
+    if (!AppOperate[info[0]]) return;
     if (!device.isScreenOn()) startStep(unlockPhone);
     toast("开始执行");
-    operate[info.shift()](info);
+    AppOperate[info.shift()](info);
+    n.delete();
 });
 
 function test() {
     const info = ["QQ", "坚果的坚果", "消息1", "消息2", "消息3"];
     if (!device.isScreenOn()) startStep(unlockPhone);
-    operate[info.shift()](info);
+    AppOperate[info.shift()](info);
 }
-// test();
+test();
